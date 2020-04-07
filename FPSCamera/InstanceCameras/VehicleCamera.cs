ece5502dea9b58d1ed6b876e8ea0b992bfc3618f
Edvin Alestig <edvin.alestig@gmail.com>
2020-04-07 14:33:22 +0200

    Updated to latest version of IPT2

--- FPSCamera/InstanceCameras/VehicleCamera.cs
+++ FPSCamera/InstanceCameras/VehicleCamera.cs
@@ -174,8 +174,8 @@ namespace FPSCamera
         public void GetLastStopExchange()
         {
             ushort firstVehicle = vManager.m_vehicles.m_buffer[(int)followInstance].GetFirstVehicle(followInstance);
-            int lastGone = ImprovedPublicTransport2.Detour.VehicleManagerMod.m_cachedVehicleData[(int)firstVehicle].LastStopGonePassengers;
-            int lastNew = ImprovedPublicTransport2.Detour.VehicleManagerMod.m_cachedVehicleData[(int)firstVehicle].LastStopNewPassengers;
+            int lastGone = ImprovedPublicTransport2.CachedVehicleData.m_cachedVehicleData[(int)firstVehicle].LastStopGonePassengers;
+            int lastNew = ImprovedPublicTransport2.CachedVehicleData.m_cachedVehicleData[(int)firstVehicle].LastStopNewPassengers;
             FPSCameraSpeedUI.Instance.lastExchange = String.Format("Last stop: -{0} | +{1}", lastGone, lastNew);
         }
 
