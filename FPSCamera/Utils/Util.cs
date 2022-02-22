2f3921ca41dbb1f4abce12d09916e29674c1c1e7
Hao-Chen Su <g0010726@gmail.com>
2022-02-22 07:06:18 +0100

    remove unused func in Util

--- FPSCamera/Utils/Util.cs
+++ FPSCamera/Utils/Util.cs
@@ -4,43 +4,8 @@ using System.Reflection;
 
 namespace FPSCamera
 {
-    /// <summary>
-    /// A Bunch of Reflection utility helpers
-    /// </summary>
     public static class Util
     {
-        public static MethodInfo FindMethod<T>(T o, string methodName)
-        {
-            var methods = typeof(T).GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            foreach (var m in methods)
-            {
-                if (m.Name == methodName){
-                    return m;
-                }
-            }
-            return null;
-        }
-
-        public static FieldInfo FindField<T>(T o, string fieldName)
-        {
-            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-
-            foreach (var f in fields)
-            {
-                if (f.Name == fieldName)
-                {
-                    return f;
-                }
-            }
-
-            return null;
-        }
-
-        public static T GetFieldValue<T>(FieldInfo field, object o)
-        {
-            return (T)field.GetValue(o);
-        }
-
         public static Q ReadPrivate<T, Q>(T o, string fieldName)
         {
             var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
@@ -56,28 +21,6 @@ namespace FPSCamera
             }
 
             return (Q)field.GetValue(o);
-        }
-
-        public static void WritePrivate<T, Q>(T o, string fieldName, object value)
-        {
-            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            FieldInfo field = null;
-
-            foreach (var f in fields)
-            {
-                if (f.Name == fieldName)
-                {
-                    field = f;
-                    break;
-                }
-            }
-
-            field.SetValue(o, value);
-        }
-
-        public static ulong[] GetUserModsList()
-        {
-            return PluginManager.instance.GetPluginsInfo().Where(plugin => plugin.isEnabled).Select(info => info.publishedFileID.AsUInt64).ToArray();
         }
     }
 
