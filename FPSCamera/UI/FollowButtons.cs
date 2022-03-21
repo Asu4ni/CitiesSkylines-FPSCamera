abfe82477933d7e05803068368bab1788d979253
Hao-Chen Su <g0010726@gmail.com>
2022-03-21 19:07:26 +0100

    Make follow button in WorldInfoPanel invisible when it's parked car

--- FPSCamera/UI/FollowButtons.cs
+++ FPSCamera/UI/FollowButtons.cs
@@ -1,54 +1,79 @@
 namespace FPSCamera.UI
 {
     using ColossalFramework.UI;
+    using System;
+    using System.Collections.Generic;
     using UnityEngine;
+    using ID = Wrapper.ID;
 
     internal class FollowButtons : MonoBehaviour
     {
         private static readonly Vector3 btnOffset = new Vector3(-4f, -20f, 0f);
         private static readonly Utils.Size2D btnSize = new Utils.Size2D(30f, 30f);
 
-        internal void registerFollowCallBack(System.Action<Wrapper.ID> callBackAction)
+        internal void registerFollowCallBack(System.Action<ID> callBackAction)
         { followCallBack = callBackAction; }
 
         private void Awake()
         {
-            _buttons = new UIButton[]
-            {
-                CreateFollowBtn<CitizenVehicleWorldInfoPanel>(),
-                CreateFollowBtn<CityServiceVehicleWorldInfoPanel>(),
-                CreateFollowBtn<PublicTransportVehicleWorldInfoPanel>(),
-                CreateFollowBtn<CitizenWorldInfoPanel>(),
-                CreateFollowBtn<TouristWorldInfoPanel>()
-            };
+            _infoPanels = new List<InfoPanel>();
+
+            System.Func<ID, bool> always = (_) => true;
+
+            CreateFollowBtn<CitizenVehicleWorldInfoPanel>((id) => !(id is Wrapper.ParkedCarID));
+            CreateFollowBtn<CityServiceVehicleWorldInfoPanel>(always);
+            CreateFollowBtn<PublicTransportVehicleWorldInfoPanel>(always);
+            CreateFollowBtn<CitizenWorldInfoPanel>(always);
+            CreateFollowBtn<TouristWorldInfoPanel>(always);
         }
+
+        private void LateUpdate()
+        {
+            foreach (var p in _infoPanels)
+                p.followButton.isVisible = _GetID(p.panel) is ID id && p.filter(id);
+        }
+
         private void OnDestroy()
         {
-            foreach (var btn in _buttons)
-                if (btn != null) Destroy(btn);
+            foreach (var p in _infoPanels)
+                if (p.panel != null) Destroy(p.panel);
         }
 
-        private UIButton CreateFollowBtn<Panel>() where Panel : WorldInfoPanel
+        private void CreateFollowBtn<Panel>(System.Func<ID, bool> filter)
+                                            where Panel : WorldInfoPanel
         {
             var panel = UIView.library.Get<Panel>(typeof(Panel).Name);
-
-            var btn = panel.component.AsParent().AddSpriteButton("StartFollow", btnSize, Helper.GetClickHandler((_) => {
-                followCallBack(Wrapper.ID.FromGame(
-                             Utils.ReadFields(panel).Get<InstanceID>("m_InstanceID")));
-                panel.Hide();
-                return true;
-            }), "Start Follow Mode", scale: .8f);
+            var btn = panel.component.AsParent().AddSpriteButton("StartFollow", btnSize,
+                          Helper.GetClickHandler((_) => {
+                              if (_GetID(panel) is ID id && filter(id)) {
+                                  followCallBack(id);
+                                  panel.Hide();
+                                  return true;
+                              }
+                              return false;
+                          }), "Start Follow Mode", scale: .8f);
             btn.AlignTo(panel.component, UIAlignAnchor.BottomRight);
             btn.relativePosition += btnOffset;
-            return btn;
+            _infoPanels.Add(new InfoPanel(panel, btn, filter));
         }
 
-        internal void OnCamDeactivate() { foreach (var btn in _buttons) btn.Enable(); }
-        internal void OnCamActivate() { foreach (var btn in _buttons) btn.Disable(); }
+        internal void OnCamDeactivate() { foreach (var p in _infoPanels) p.followButton.Enable(); }
+        internal void OnCamActivate() { foreach (var p in _infoPanels) p.followButton.Disable(); }
 
-        private UIButton[] _buttons;
-        private System.Action<Wrapper.ID> _followCallBack;
-        private System.Action<Wrapper.ID> followCallBack {
+        private static ID _GetID(WorldInfoPanel panel)
+            => ID.FromGame(Utils.ReadFields(panel).Get<InstanceID>("m_InstanceID"));
+
+        struct InfoPanel
+        {
+            public readonly WorldInfoPanel panel;
+            public readonly UIButton followButton;
+            public readonly System.Func<ID, bool> filter;
+            public InfoPanel(WorldInfoPanel panel, UIButton followButton, Func<ID, bool> filter)
+            { this.panel = panel; this.followButton = followButton; this.filter = filter; }
+        }
+        private List<InfoPanel> _infoPanels;
+        private System.Action<ID> _followCallBack;
+        private System.Action<ID> followCallBack {
             get {
                 if (_followCallBack is null)
                     Log.Err("followCallBack from GamePanelExtender has not been registered");
